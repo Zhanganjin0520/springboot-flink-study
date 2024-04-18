82157e79dfe5504139952676d6b23ad30efaa3ad
Zhanganjin0520 <43087612+Zhanganjin0520@users.noreply.github.com>
2024-04-18 09:29:58 +0800

    替换 keyBy 的用法

--- src/main/java/com/geespace/flink/springbootflinkstudy/workcount/WordCountStreamDemo.java
+++ src/main/java/com/geespace/flink/springbootflinkstudy/workcount/WordCountStreamDemo.java
@@ -1,16 +1,22 @@
 package com.geespace.flink.springbootflinkstudy.workcount;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.typeutils.TupleTypeInfo;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
+@Slf4j
 public class WordCountStreamDemo {
 
     public static void main(String[] args) throws Exception {
         // 创建流执行环境
-        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        final StreamExecutionEnvironment env
+                = StreamExecutionEnvironment.createRemoteEnvironment("10.139.106.33", 8081, "C:\\Users\\Zhangaj\\IdeaProjects\\springboot-flink-study\\target\\springboot-flink-study-0.0.1-SNAPSHOT.jar");
 
         // 输入数据源，这里使用一个固定的字符串数组作为输入流
         String[] lines = {"hello world", "hello flink", "world world"};
@@ -21,29 +27,32 @@ public class WordCountStreamDemo {
         // 单词拆分并计数
         DataStream<Tuple2<String, Integer>> wordCounts = textStream
                 // 将每行文本按空格拆分成单词
-                .flatMap(new Tokenizer())
-                // 根据单词进行分组
-                .keyBy(0)
-                // 计算单词出现的次数
-                .sum(1);
+                .flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+                    @Override
+                    public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+                        // 按空格拆分单词
+                        for (String word : value.split("\\s")) {
+                            // 发送单词及其出现次数为1的元组
+                            out.collect(new Tuple2<>(word, 1));
+                        }
+                    }
 
+                })
+                // 根据单词进行分组
+                .keyBy(new KeySelector<Tuple2<String, Integer>, Object>() {
+                    @Override
+                    public Object getKey(Tuple2<String, Integer> value) throws Exception {
+                        return value.f0;
+                    }
+                })
+                // 计算单词出现的次数
+                .sum(1)
+                .returns(Types.TUPLE(Types.STRING, Types.INT));
         // 输出结果
         wordCounts.print();
 
         // 执行任务
         env.execute("WordCount Stream Demo");
-    }
-
-    // 自定义拆分函数，用于将文本按空格拆分成单词
-    public static final class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
-        @Override
-        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
-            // 按空格拆分单词
-            for (String word : value.split("\\s")) {
-                // 发送单词及其出现次数为1的元组
-                out.collect(new Tuple2<>(word, 1));
-            }
-        }
     }
 }
 
--- src/test/java/com/geespace/flink/springbootflinkstudy/SpringbootFlinkStudyApplicationTests.java
+++ src/test/java/com/geespace/flink/springbootflinkstudy/SpringbootFlinkStudyApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringbootFlinkStudyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
